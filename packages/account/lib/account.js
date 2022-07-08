f845d4f79c8c28a9a3831ba63b80b228031e0a56
duck59uet <ducnv1911@gmail.com>
2022-07-08 11:17:50 +0700

    Up code

--- packages/account/lib/account.js
+++ packages/account/lib/account.js
@@ -1,18 +1,22 @@
-'use strict';
+"use strict";
 
-var bip39 = require('bip39')
+var bip39 = require("bip39");
+const stargate = require("@cosmjs/stargate");
+const amino = require("@cosmjs/amino");
 
 module.exports = {
-    account,
-    generateAccount
+  generateAccount
 };
 
-function account() {
-    // TODO
+async function generateAccount() {
+  // Generate from mnemonic
+  let mnemonic = bip39.generateMnemonic();
+  let privateKey = await amino.Secp256k1HdWallet.fromMnemonic(mnemonic, {
+    prefix: "aura"
+  });
+  let pubkeys = amino.encodeSecp256k1Pubkey(await privateKey.getAccounts()[0].pubkey);
+  let address = (await wallet.getAccounts())[0].address;
+
+  console.log(pubkeys);
 }
 
-function generateAccount(){
-    // Generate from mnemonic 
-    const mnemonic = bip39.generateMnemonic();
-    console.log(mnemonic.toString());
-}
